059010abd7ce708d4d98df2f4ad547cf9cad1153
peanutyabing <sherry.liuyabing@gmail.com>
2023-04-12 17:47:48 +0800

    Implement auth on listing page and include access token and buyer email in put request

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -3,12 +3,15 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-
 import { BACKEND_URL } from "../constants.js";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Listing = () => {
   const [listingId, setListingId] = useState();
   const [listing, setListing] = useState({});
+  const [accessToken, setAccessToken] = useState("");
+  const { isAuthenticated, user, loginWithRedirect, getAccessTokenSilently } =
+    useAuth0();
 
   useEffect(() => {
     // If there is a listingId, retrieve the listing data
@@ -19,6 +22,18 @@ const Listing = () => {
     }
     // Only run this effect on change to listingId
   }, [listingId]);
+
+  const checkUser = async () => {
+    if (isAuthenticated) {
+      let token = await getAccessTokenSilently();
+      console.log(token);
+      setAccessToken(token);
+    }
+  };
+
+  useEffect(() => {
+    checkUser();
+  }, []);
 
   // Update listing ID in state if needed to trigger data retrieval
   const params = useParams();
@@ -36,10 +51,24 @@ const Listing = () => {
     }
   }
 
-  const handleClick = () => {
-    axios.put(`${BACKEND_URL}/listings/${listingId}`).then((response) => {
-      setListing(response.data);
-    });
+  const handleClick = async () => {
+    if (isAuthenticated) {
+      axios
+        .put(
+          `${BACKEND_URL}/listings/${listingId}`,
+          { buyerEmail: user.email },
+          { headers: { Authorization: `Bearer ${accessToken}` } }
+        )
+        .then((response) => {
+          setListing(response.data);
+        });
+    } else {
+      await loginWithRedirect({
+        authorizationParams: {
+          redirect_uri: `http://localhost:3001/listings/${listingId}`,
+        },
+      });
+    }
   };
 
   return (
@@ -48,7 +77,10 @@ const Listing = () => {
       <Card bg="dark">
         <Card.Body>
           {listingDetails}
-          <Button onClick={handleClick} disabled={listing.BuyerId}>
+          <Button
+            onClick={handleClick}
+            // disabled={listing.BuyerId}
+          >
             Buy
           </Button>
         </Card.Body>
